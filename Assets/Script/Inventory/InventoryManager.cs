2a786ed8d25469d0f0ad7ff268c16b591633e5b1
KelsGaming <alcorizamichael@gmail.com>
2024-10-18 00:19:16 +0800

    Create Iventory Manager class and ItemPickup Script

---
+++ Assets/Script/Inventory/InventoryManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : SingletonMonoBehavior<InventoryManager>
+{
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;
+    [SerializeField] private SO_ItemList itemList = null;
+
+    private void Start()
+    {
+        CreateItemDetailDictionary();
+    }
+
+    private void CreateItemDetailDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+        foreach(ItemDetails itemDetails in itemList.itemDetails)
+        {
+            itemDetailsDictionary.Add(itemDetails.itemCode, itemDetails);
+        }
+    }
+    public ItemDetails GetItemDetails(int ItemCode)
+    {
+        ItemDetails itemDetails;
+
+        if(itemDetailsDictionary.TryGetValue(ItemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
